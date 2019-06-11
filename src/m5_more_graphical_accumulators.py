3aa1d4db18b71fae82be9e033fa3340562b419f2
dohertre <dohertre@rose-hulman.edu>
2019-06-11 16:28:49 -0400

    random

--- src/m5_more_graphical_accumulators.py
+++ src/m5_more_graphical_accumulators.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python. 
   
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Rebekah Doherty.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -111,7 +111,23 @@ def draw_squares_from_circle(n, circle, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ####################################################################
     # ------------------------------------------------------------------
+    x = point.x
+    y = point.y
 
+    point = rg.Point(x, y)
+    circle = rg.Circle(point, radius)
+
+    circle.attach_to(window)
+
+    # for k in range(n):
+    #     point = rg.Point(x, y)
+    #     circle = rg.Circle(point, radius)
+    #
+    #     circle.attach_to(window)
+    #
+    #     x = x + (radius * 2)
+
+    window.render()
 
 def run_test_draw_circles_from_rectangle():
     """ Tests the   draw_circles_from_rectangle  function. """
